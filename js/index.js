408b9eddfc5aaffb06b84a5960cb2e4a70568593
Tamira2804 <tamira2804@gmail.com>
2023-05-21 14:51:32 +0200

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+/*
+  1. Основи подій. Створення та видалення слухачів
+
+  Опис та передача колбеків інлайново та як окремі функції, різниця
+
+  Найменування колбеків для слухачів:
+  - handle*: handleSubjectEvent
+  - *Handler: subjectEventHandler
+  - on*: onSubjectEvent
+
+  show the correct one)
+
+  Реалізувати функціонал підміни зображення при кліці на кнопку
+  та прибирання слухача події при кліці на іншу
+*/
+
+// https://picsum.photos/200/300
+
+// const imgEl = document.querySelector(".js-img");
+
+// const swapBtnEl = document.querySelector(".btn.js-swap-btn");
+
+// const deleteListenerBtnEl = document.querySelector(".btn.js-remove-listener");
+
+// console.log(imgEl);
+// console.log(swapBtnEl);
+// console.log(deleteListenerBtnEl);
+
+// const handleSwapBtnClick = () => {
+//   imgEl.src = "https://picsum.photos/200/300";
+// };
+
+// swapBtnEl.addEventListener("click", handleSwapBtnClick);
+
+// deleteListenerBtnEl.addEventListener("click", () => {
+//   swapBtnEl.removeEventListener("click", handleSwapBtnClick);
+// });
